a3c90424492330de8727742444763a9c90123c7d
SheetalPawarAuty <102964955+SheetalPawarAuty@users.noreply.github.com>
2022-04-20 14:19:46 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+alert('Hello World!');
+
+//Assigning and Changing values of the variable
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+//Addition
+let simpleAddition = 2+2;
+document.write(simpleAddition);
+
+//Multiplication
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+
+//Doubling your doubleSize variable and subtracting half of the value of your original size variable
+let minSize = (doubleSize*2) - (size/2);
+document.write(minSize);
