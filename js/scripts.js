63dcbe5bda04c6e2aea6ad6fbdb6e790697c5677
SheetalPawarAuty <102964955+SheetalPawarAuty@users.noreply.github.com>
2022-04-21 10:41:43 +0200

    Update scripts.js
    If applied, this commit will have several objects added to the Array pokemonList.  Each object has 3 keys: name, height and type.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
-let pokemonList = [];
+let pokemonList = [
+  {name: 'pikachu', height: 4, type: ['field','fairy']},
+  {name: 'bulbasaur', height: 7, type: ['monster','grass']},
+  {name: 'paras', height: 3, type: ['bug','grass']},
+  {name: 'squirtle', height: 5, type: ['monster','water']},
+  {name: 'seel', height: 11, type: ['water','field']}
+];
