d7ffc0b4cf67c369de401dfc795b723233618f70
SheetalPawarAuty <102964955+SheetalPawarAuty@users.noreply.github.com>
2022-04-22 08:26:29 +0200

    Update scripts.js
    If applied, this commit will have indentation changed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,7 @@
 let pokemonList = [
-  {
-  name: 'pikachu',
-  height: 4,
-  types: ['field','fairy']
-},
-  {
-  name: 'bulbasaur',
-  height: 7,
-  types: ['monster','grass']
-},
-  {
-  name: 'paras',
-  height: 3,
-  types: ['bug','grass']
-},
-  {
-  name: 'squirtle',
-  height: 5,
-  types: ['monster','water']
-},
-  {
-  name: 'seel',
-  height: 11,
-  types: ['water','field']
-}
+  {name: 'pikachu', height: 4, types: ['field','fairy']},
+  {name: 'bulbasaur', height: 7, types: ['monster','grass']},
+  {name: 'paras', height: 3, types: ['bug','grass']},
+  {name: 'squirtle', height: 5, types: ['monster','water']},
+  {name: 'seel', height: 11, types: ['water','field']}
 ];
